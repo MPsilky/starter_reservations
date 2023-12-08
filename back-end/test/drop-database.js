f5c4f4b11f2be63d5b15ef4b62fb04719a0d137a
Katana Coleman <131917030+MPsilky@users.noreply.github.com>
2023-12-08 00:38:39 -0500

    Update drop-database.js

--- back-end/test/drop-database.js
+++ back-end/test/drop-database.js
@@ -1,14 +1,23 @@
 const path = require("path");
 
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+// Update the path to use .env.test for the test environment variables
+require("dotenv").config({ path: path.join(__dirname, "..", ".env.test") });
 
 const knex = require("../src/db/connection");
 
-knex.migrate
-  .forceFreeMigrationsLock()
+// Force release any existing migration locks
+knex.migrate.forceFreeMigrationsLock()
+  // Rollback all migrations to clean the database
   .then(() => knex.migrate.rollback(null, true))
+  // Run the latest migrations to re-create the schema
   .then(() => knex.migrate.latest())
+  // Re-seed the database with initial data
   .then(() => knex.seed.run())
-  .then(() => console.log("Dropped and seeded database"))
-  .then(() => knex.destroy())
-  .catch((error) => console.error("Failed to drop and seed database", error));
+  .then(() => {
+    console.log("Dropped all tables, re-run migrations, and re-seeded the database");
+    return knex.destroy();
+  })
+  .catch((error) => {
+    console.error("Failed to reset the test database", error);
+    return knex.destroy();
+  });
